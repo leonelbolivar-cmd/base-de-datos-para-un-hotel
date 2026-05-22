05512eda15fa60dc9b534d2c18610192329a88e4
diegomezaperochena <diego.meza.perochena@ucsp.edu.pe>
2026-05-22 09:48:55 -0500

    feat: Registros módulo 1

--- datos.sql
+++ datos.sql
@@ -1,3 +1,22 @@
+-- Modulo 1: ESTRUCTURA INTERNA Y PERSONAL
+
+INSERT INTO departamentos (id_departamento, nombre_area, anexo_telefonico)
+VALUES 
+  (10, 'Cocina', '101'),
+  (20, 'Administracion', '102'),
+  (30, 'Limpieza', '103'),
+  (40, 'Seguridad', '104'),
+  (50, 'Guardería', '105');
+
+
+INSERT INTO empleados (id_empleado, nombre_completo, cargo, turno, id_departamento, id_jefe_directo)
+VALUES
+  (1, 'Miguel Velazquez Vega', 'Chef', 'Mañana', 10, 5),
+  (2, 'Ángel Auxilio Mena', 'Vigilante', 'Noche', 40, 3),
+  (3, 'Paco Vega Osmos', 'Vigilante', 'Mañana', 40, NULL),
+  (4, 'Susana Oria Ramos', 'Recepcionista', 'Mañana', 20, 6),
+  (5, 'Tomas Castelo Pishy', 'Conserje', 'Tarde', 30, 7);
+
 --Modulo 3--
 
 INSERT INTO proveedores(ruc_proveedor,razon_social,telefono_contacto)
