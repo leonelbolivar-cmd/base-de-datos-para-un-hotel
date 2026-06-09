38e686cd475191fde7170bccc3c7907c9a1b7d06
rodloucs <rodrigo.lovon@ucsp.edu.pe>
2026-06-09 11:40:31 -0500

    feat: endpoint

--- endpoints.sql
+++ endpoints.sql
@@ -9,3 +9,16 @@ INNER JOIN restaurante_consumos r ON c.id_reserva = r.id_reserva
 GROUP BY c.dni_cliente, c.nombre_completo
 HAVING COUNT(r.id_consumo) >= 3 -- Condición avanzada: clientes que consumen frecuentemente
 ORDER BY "Gasto_consumo" DESC;
+
+
+SELECT 
+    h.tipo_habitacion ,
+    COUNT(r.id_reserva) ,
+    SUM(b.monto_total) AS 'Ingresos_totales'
+FROM habitaciones h
+INNER JOIN reserva r ON h.numero_habitacion = r.numero_habitacion
+INNER JOIN boleta b ON r.id_reserva = b.id_reserva
+WHERE r.estado = 'Finalizada' 
+GROUP BY h.tipo_habitacion
+HAVING SUM(b.monto_total) > 500.00
+ORDER BY 'Ingresos_totales'DESC;
